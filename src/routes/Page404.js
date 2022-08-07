5165a475a198eed201c5f70faf55aa93e4bb33a1
Milan Stojadinovic <milan.stojadinovic.77@gmail.com>
2022-08-07 18:45:35 +0200

    partially completed

--- src/routes/Page404.js
+++ src/routes/Page404.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navigation from './Navigation';
 import './Page404.css';
 
 const Page404 = () => {
   return (
-    <div className='not-found'>
-      <h1 className='not-found__main-title'>404 - NOT FOUND</h1>
+    <div>
+      <Navigation />
+      <div className='not-found'>
+        <h1 className='not-found__main-title'>404 - NOT FOUND</h1>
+      </div>
     </div>
   );
 };
