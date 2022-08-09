fc3ae30dbb4e84c9d028252f075488b3fa1e6ac7
Milan Stojadinovic <milan.stojadinovic.77@gmail.com>
2022-08-09 10:28:29 +0200

    random rgb in Page404

--- src/components/routes/Page404.js
+++ src/components/routes/Page404.js
@@ -3,11 +3,22 @@ import Navbar from '../routes/Navbar';
 import './Page404.css';
 
 const Page404 = () => {
+  //random font color function
+  const randomRGB = () => {
+    let R = Math.trunc(Math.random() * 255);
+    let G = Math.trunc(Math.random() * 255);
+    let B = Math.trunc(Math.random() * 255);
+
+    return `rgb(${R}, ${G}, ${B})`;
+  };
+
   return (
     <div>
       <Navbar />
       <div className='not-found'>
-        <h1 className='not-found__main-title'>404 - NOT FOUND</h1>
+        <h1 className='not-found__main-title' style={{ color: randomRGB() }}>
+          404 - NOT FOUND
+        </h1>
       </div>
     </div>
   );
