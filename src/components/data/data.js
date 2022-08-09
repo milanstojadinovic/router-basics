8620629ff91cbc1935f9b3bdad547655c76ae971
Milan Stojadinovic <milan.stojadinovic.77@gmail.com>
2022-08-09 10:17:06 +0200

    added data dir

---
+++ src/components/data/data.js
@@ -0,0 +1,23 @@
+const data = [
+  {
+    id: 1,
+    title: 'About my hobbies',
+    text: 'my hobbies.....',
+  },
+  {
+    id: 2,
+    title: 'About my work',
+    text: 'I worked in ....',
+  },
+  {
+    id: 3,
+    title: 'About childhood',
+    text: 'I was born in ....',
+  },
+];
+
+const getData = () => {
+  return data;
+};
+
+export default getData;
--- src/components/routes/About.js
+++ src/components/routes/About.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import Header from '../UI/Header';
 import Navbar from '../routes/Navbar';
+import getData from '../data/data';
 
 import './About.css';
 
 const About = () => {
+  const aboutMe = getData();
+
   return (
     <div>
       <Navbar />
       <Header className='about'>
         <h1 className='about__main-title'>About Me Page</h1>
       </Header>
+      {aboutMe.map((about) => (
+        <div className='about-item' key={about.id}>
+          <h4 className='about-item__title'>{about.title}</h4>
+          <p className='about-item__text'>{about.text}</p>
+        </div>
+      ))}
     </div>
   );
 };
